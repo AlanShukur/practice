68af6edc56f476613dda1c22a3135ba74c26d7ee
Top-shagger <alan_shukur1@hotmail.com>
2025-03-25 09:26:12 +0900

    practice scanners and ohter print methods

--- main.c
+++ main.c
@@ -8,6 +8,21 @@ int main(){
   printf("%f\n", 3.14);
   printf("%.2f\n", 3.14159);
   printf("%c\n", 'A');
+
+  printf("%10d\n", 123);
+  printf("%-10d\n", 123);
+
+  printf("%.2f\n", 3.14159);
+
+  printf("%05d\n", 42);
  //test
+
+
+  int age;
+  float height;
+  printf("Enter your age and height: ");
+
+  scanf("%d %f", &age, &height);
+  printf("You are %d years old and %.2f meters tall.\n", age, height);
   return 0;
 }
